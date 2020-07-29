ace2b95a3ca621396d4dc8db614600c96866bc24
Salsabila Nadhifah <salsanads@gmail.com>
2020-07-29 22:21:02 +0700

    Add default value of env

--- undercover/__init__.py
+++ undercover/__init__.py
@@ -4,7 +4,7 @@ from .game_state import GameState, Status
 from .models import init_db
 
 
-def main(db_url, env):
+def main(db_url, env="dev"):
     echo = env != "prod"
     engine = create_engine(db_url, echo=echo)
     init_db(engine)
