a8d2f86ce724a9dd90fb29623d7c731c9131bfec
Ghilman Al Fatih <ghilman27@pop-os.localdomain>
2020-07-30 23:56:29 +0700

    add new DB model: Player

--- undercover/models/__init__.py
+++ undercover/models/__init__.py
@@ -1,3 +1,4 @@
 from .database import init_db
+from .player import Player
 from .playing_role import PlayingRole
 from .secret_word import SecretWord
---
+++ undercover/models/player.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Boolean, Column, ForeignKeyConstraint, String
+from sqlalchemy.orm import relationship
+
+from .database import Base, add_session
+from .playing_role import PlayingRole
+
+
+class Player(Base):
+    __tablename__ = "player"
+
+    user_id = Column(String, primary_key=True)
+    alive = Column(Boolean, nullable=False)
+    guessing = Column(Boolean, nullable=False)
+    room_id = Column(String, primary_key=True)
+    role = Column(String, nullable=False)
+    __table_args__ = (
+        ForeignKeyConstraint(
+            [room_id, role], [PlayingRole.room_id, PlayingRole.role]
+        ),
+        {},
+    )
+
+    @classmethod
+    @add_session
+    def insert(cls, session, player):
+        session.add(player)
+        session.commit()
