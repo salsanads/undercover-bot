21e5a5dc5e8866af29cb0914ac99def46a58ca7c
Salsabila Nadhifah <9564160+salsanads@users.noreply.github.com>
2020-07-31 18:11:40 +0700

    Update query to be more readable

--- undercover/models/playing_role.py
+++ undercover/models/playing_role.py
@@ -18,11 +18,8 @@ class PlayingRole(Base):
     @classmethod
     @add_session
     def exists(cls, room_id, session):
-        subquery = session.query(cls).filter_by(room_id=room_id)
-        exists = (
-            session.query(literal(True)).filter(subquery.exists()).scalar()
-        )
-        return exists is not None
+        room_id = session.query(cls.room_id).filter_by(room_id=room_id).first()
+        return room_id is not None
 
     @classmethod
     @add_session
