22b3ab5cae79115b6ac6f10aa24686725bf2aaef
Ghilman Al Fatih <ghilman27@pop-os.localdomain>
2020-08-01 13:02:18 +0700

    remove unused import

--- undercover/models/player.py
+++ undercover/models/player.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Boolean, Column, ForeignKeyConstraint, String
-from sqlalchemy.orm import relationship
 
 from .database import Base, add_session
 from .playing_role import PlayingRole
