198a7044b9439d722e678824943339e19bbdd6c2
Ghilman AL Fatih <ghilman27@gmail.com>
2020-08-02 17:49:16 +0700

    change output type of ongoing_game_found to follow new convention

--- undercover/controllers/helpers.py
+++ undercover/controllers/helpers.py
@@ -11,9 +11,9 @@ def ongoing_game_found(should_be_found):
         def wrapper(room_id, *args, **kwargs):
             actual_found = PlayingRole.exists(room_id)
             if not should_be_found and actual_found:
-                return GameState(Status.ONGOING_GAME_FOUND)
+                return [GameState(Status.ONGOING_GAME_FOUND)]
             elif should_be_found and not actual_found:
-                return GameState(Status.ONGOING_GAME_NOT_FOUND)
+                return [GameState(Status.ONGOING_GAME_NOT_FOUND)]
             return func(room_id, *args, **kwargs)
 
         return wrapper
