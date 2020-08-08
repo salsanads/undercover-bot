af6424c48e419d3d34d37374e1829e6a30094d4d
Salsabila Nadhifah <9564160+salsanads@users.noreply.github.com>
2020-08-08 13:40:56 +0700

    Set system time as random seed

--- undercover/__init__.py
+++ undercover/__init__.py
@@ -1,3 +1,6 @@
+import random
+from datetime import datetime
+
 from sqlalchemy import create_engine
 
 from .models import init_db
@@ -8,3 +11,5 @@ def main(db_url, env="dev"):
     echo = env != "prod"
     engine = create_engine(db_url, echo=echo)
     init_db(engine)
+
+    random.seed(datetime.now())
