91bcf44915c098ab59fd93f83a1a68a93ed77ff9
Arkaan <arkaan@ark.laptop>
2020-08-16 23:21:54 +0700

    add distance measuring

--- undercover/controllers/guess_controller.py
+++ undercover/controllers/guess_controller.py
@@ -1,3 +1,5 @@
+import Levenshtein
+
 from undercover import GameState, Role, Status
 from undercover.models import Player, PlayingRole
 
@@ -10,7 +12,9 @@ def guess_word(user_id, word):
         return [GameState(Status.NOT_IN_GUESSING_TURN)]
 
     civilian_word = PlayingRole.get_word(player.room_id, Role.CIVILIAN)
-    if word.lower() == civilian_word.word.lower():
+    if word.lower() == civilian_word.word.lower() or Levenshtein.distance(
+        word.lower(), civilian_word.word.lower()
+    ) < int((len(civilian_word.word)) / 10):
         clear_game(player.room_id)
         return [GameState(Status.MR_WHITE_WIN, room_id=player.room_id)]
 
