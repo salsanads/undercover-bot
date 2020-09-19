62b23f5e0e94229723da76379e9ed2f07940621f
Ghilman AL Fatih <ghilman27@gmail.com>
2020-09-19 14:25:00 +0700

    rename vote_id to voter_id

--- undercover/tests/models/test_poll_vote.py
+++ undercover/tests/models/test_poll_vote.py
@@ -12,8 +12,8 @@ class TestPollVote:
     def populate_db(session):
         for poll_id, room_id in EXISTING_POLLS:
             session.add(Poll(poll_id, room_id))
-        for vote_id, poll_id, voted_user_id in EXISTING_VOTES:
-            session.add(Vote(vote_id, poll_id, voted_user_id))
+        for voter_id, poll_id, voted_user_id in EXISTING_VOTES:
+            session.add(Vote(voter_id, poll_id, voted_user_id))
         session.commit()
         yield
         session.query(Poll).delete()
@@ -32,13 +32,13 @@ class TestPollVote:
     @staticmethod
     def test_add_vote(session):
         poll_id = 3
-        vote_id = 4
+        voter_id = 4
         voted_user_id = 4
-        Vote.add(Vote(vote_id, poll_id, voted_user_id))
-        vote = session.query(Vote).filter_by(vote_id=vote_id).first()
+        Vote.add(Vote(voter_id, poll_id, voted_user_id))
+        vote = session.query(Vote).filter_by(voter_id=voter_id).first()
 
         assert vote.poll_id == poll_id
-        assert vote.vote_id == vote_id
+        assert vote.voter_id == voter_id
         assert vote.voted_user_id == voted_user_id
 
     @staticmethod
@@ -50,19 +50,19 @@ class TestPollVote:
 
     @staticmethod
     def test_add_duplicate_vote():
-        vote_id = 1
+        voter_id = 1
         poll_id = 2
         voted_user_id = 3
         with pytest.raises(Exception):
-            Vote.add(Vote(vote_id, poll_id, voted_user_id))
+            Vote.add(Vote(voter_id, poll_id, voted_user_id))
 
     @staticmethod
     def test_delete(session):
-        vote_id, _, _ = EXISTING_VOTES[0]
+        voter_id, _, _ = EXISTING_VOTES[0]
         poll_id, _ = EXISTING_POLLS[0]
-        Vote.delete(vote_id)
+        Vote.delete(voter_id)
         Poll.delete(poll_id)
-        vote = session.query(Vote).get(vote_id)
+        vote = session.query(Vote).get(voter_id)
         poll = session.query(Poll).get(poll_id)
 
         assert vote is None
@@ -80,13 +80,13 @@ class TestPollVote:
         votes = Vote.find_all(poll_id=1)
         assert len(votes) == 2
         for vote in votes:
-            print(vote.vote_id, vote.poll_id, vote.voted_user_id)
+            print(vote.voter_id, vote.poll_id, vote.voted_user_id)
 
     @staticmethod
     def test_update_vote():
         value = 3
-        for vote_id, _, _ in EXISTING_VOTES:
-            Vote.update(vote_id, voted_user_id=value)
+        for voter_id, _, _ in EXISTING_VOTES:
+            Vote.update(voter_id, voted_user_id=value)
 
         votes = Vote.find_all()
         for vote in votes:
