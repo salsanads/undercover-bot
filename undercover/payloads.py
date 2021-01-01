db62e1b9945b667f6383ebf998a1be7c6d37e0d4
Salsabila Nadhifah <9564160+salsanads@users.noreply.github.com>
2021-01-01 21:56:51 +0700

    Re-order payloads status for clear separation

--- undercover/payloads.py
+++ undercover/payloads.py
@@ -15,33 +15,41 @@ class GameState:
 
 
 class Status(Enum):
-    # HAPPY PATH
+    # start
     PLAYED_WORD = auto()
     PLAYING_ORDER = auto()
+    INVALID_PLAYER_NUMBER = auto()
+    PLAYING_USER_FOUND = auto()
+
+    # eliminate
     CIVILIAN_ELIMINATED = auto()
     UNDERCOVER_ELIMINATED = auto()
     MR_WHITE_ELIMINATED = auto()
+
+    # guess
     ASK_GUESSED_WORD = auto()
+    NOT_IN_GUESSING_TURN = auto()
+
+    # win
     CIVILIAN_WIN = auto()
     NON_CIVILIAN_WIN = auto()
     MR_WHITE_WIN = auto()
     SUMMARY = auto()
+
+    # poll
     POLL_STARTED = auto()
     POLL_DECIDED = auto()
     VOTE_SUCCESS = auto()
     TOTAL_VOTES_REACHED = auto()
-
-    # UNHAPPY PATH
-    INVALID_PLAYER_NUMBER = auto()
-    ONGOING_GAME_FOUND = auto()
     ONGOING_POLL_FOUND = auto()
-    PLAYING_USER_FOUND = auto()
-    ONGOING_GAME_NOT_FOUND = auto()
     ONGOING_POLL_NOT_FOUND = auto()
-    PLAYER_NOT_FOUND = auto()
-    PLAYER_ALREADY_KILLED = auto()
-    NOT_IN_GUESSING_TURN = auto()
     NO_VOTES_SUBMITTED = auto()
     NOT_ENOUGH_VOTES = auto()
     MULTIPLE_PLAYERS_VOTED = auto()
     VOTE_EXISTS = auto()
+
+    # common error
+    ONGOING_GAME_FOUND = auto()
+    ONGOING_GAME_NOT_FOUND = auto()
+    PLAYER_NOT_FOUND = auto()
+    PLAYER_ALREADY_KILLED = auto()
