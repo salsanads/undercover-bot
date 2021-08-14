431d425c690f9cef5f47fb7337af729d64d1c3b9
Salsabila Nadhifah <9564160+salsanads@users.noreply.github.com>
2021-08-14 12:12:08 +0700

    Update started game instruction and customize bot prefix based on env (#24)

--- discordbot/__init__.py
+++ discordbot/__init__.py
@@ -3,5 +3,8 @@ from discordbot.cogs import *
 from discordbot.handlers import *
 
 
-def main(bot_token):
+def main(bot_token, bot_command_prefix=None):
+    if bot_command_prefix is not None:
+        bot.command_prefix = bot_command_prefix
+
     bot.run(bot_token)
